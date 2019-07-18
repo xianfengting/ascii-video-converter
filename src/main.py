49ae9d45a9775905b21e71693e1ebbaa30c98345
xianfengting <xianfengting221@163.com>
2019-07-18 10:40:08 +0800

    Add BGM convertion and output

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageFont, ImageDraw
 import argparse
 from src import ascii_tools
 import progressbar as pbar
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
 
 # 命令行输入参数处理
 parser = argparse.ArgumentParser()
@@ -14,8 +16,13 @@ parser.add_argument('-o', '--output')
 args = parser.parse_args()
 FILE = args.file
 OUTPUT = args.output
+
 RAW_PICTURES_DIR = "./tmp/pictures"
 ASCII_PICTURES_DIR = "./tmp/ascii-pictures"
+BGM_FILE_PATH = "./tmp/bgm.mp3"
+MAX_THREAD_COUNT = 16
+
+thread_lock = threading.Lock()
 
 def convert_picture_to_ascii(picture_path, output_path):
     #print("Converting \"{}\" to \"{}\"".format(picture_path, output_path))
@@ -56,26 +63,47 @@ def convert_video_to_pictures(video, output_path):
     check_for_dirs(output_path)
     os.system("ffmpeg -i {} -r 30 {}/%05d.png".format(video, output_path))
 
+def convert_video_to_BGM(video, output_file_path):
+    os.system("ffmpeg -i {} -r 30 {}".format(video, output_file_path))
+
 def convert_pictures_to_ascii_pictures(raw_pictures_path, output_path):
     check_for_dirs(output_path)
-    widgets = [pbar.Percentage(), ' ', pbar.Bar('#'), ' ', pbar.Timer(),
+    widgets = [pbar.Percentage(), " (", pbar.SimpleProgress('/'), ") ", pbar.Bar('#'), ' ', pbar.Timer(),
             ' ', pbar.ETA(), ' ', pbar.FileTransferSpeed()]
     progress_bar = pbar.ProgressBar(widgets=widgets, maxval=len(os.listdir(raw_pictures_path)))
     progress_bar.start()
     current_progress = 0
-    for raw_picture_name in os.listdir(raw_pictures_path):
-        raw_picture_path = os.path.join(raw_pictures_path, raw_picture_name)
-        if os.path.isfile(raw_picture_path):
-            output_picture_path = os.path.join(output_path, raw_picture_name)
-            convert_picture_to_ascii(raw_picture_path, output_picture_path)
-        current_progress += 1
-        progress_bar.update(current_progress)
+    with ThreadPoolExecutor(MAX_THREAD_COUNT) as executor:
+        task_list = []
+        for raw_picture_name in os.listdir(raw_pictures_path):
+            raw_picture_path = os.path.join(raw_pictures_path, raw_picture_name)
+            if os.path.isfile(raw_picture_path):
+                output_picture_path = os.path.join(output_path, raw_picture_name)
+                #convert_picture_to_ascii(raw_picture_path, output_picture_path)
+                task_list.append(executor.submit(convert_picture_to_ascii,\
+                        picture_path=raw_picture_path, output_path=output_picture_path))
+        for task in as_completed(task_list):
+            current_progress += 1
+            progress_bar.update(current_progress)
     progress_bar.finish()
+
+def convert_pictures_and_BGM_to_video(pictures_path_template, bgm_path, output_file_path):
+    #os.system("ffmpeg -i {} -r 30 -strict -2 {}".format(pictures_path_template, "./tmp/video-no-bgm.mp4"))
+    #os.system("ffmpeg -i {} -i {} -r 30 -strict -2 {}".format("./tmp/video-no-bgm.mp4",\
+    #        bgm_path, output_file_path))
+    os.system("ffmpeg -i {} -i {} -r 30 -strict -2 -vcodec mpeg4 {}".format(pictures_path_template, bgm_path, output_file_path))
 
 def check_for_dirs(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
+def delete_temp_dirs():
+    os.rmdir("./tmp")
+
 if __name__ == "__main__":
-    #convert_video_to_pictures(FILE, RAW_PICTURES_DIR)
+    convert_video_to_pictures(FILE, RAW_PICTURES_DIR)
+    convert_video_to_BGM(FILE, BGM_FILE_PATH)
     convert_pictures_to_ascii_pictures(RAW_PICTURES_DIR, ASCII_PICTURES_DIR)
+    convert_pictures_and_BGM_to_video("{}/%05d.png".format(ASCII_PICTURES_DIR),\
+            BGM_FILE_PATH, OUTPUT)
+    delete_temp_dirs()
